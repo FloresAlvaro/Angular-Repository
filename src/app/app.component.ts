9714099108db180f789a7819feb35d92ee848b01
FloresAlvaro <alvarofloresfarfan2802@gmail.com>
2023-05-01 15:02:05 -0400

    add basic counter.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '01-new-app';
+  counter: number = 0;
+
+  add(){
+    this.counter++;
+  }
+
+  minus(){
+    this.counter--;
+  }
 }
